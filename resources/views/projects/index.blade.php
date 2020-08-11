09b50f155666f9257d654a70397ab0a4cd305af4
Matt Daneshvar <matt@4thirteen.com.my>
2020-08-11 20:59:48 +0800

    Update projects

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -6,67 +6,101 @@
     <div class="container max-w-2xl mx-auto py-8 p-6">
 
         <p class="text-justify mb-10">
-            Listed below are some of the projects I'm currently most involved with in my day to day work in no particular order.
+            Listed below are some of the projects I'm currently most involved with in my day to day work in no
+            particular order.
         </p>
 
         <h2 class="text-3xl mb-2">Products</h2>
-        @include('projects.partials.thumbnail', [
-            'name' => 'Markers',
-            'role' => 'Creator',
-            'description' => 'A Google Chrome extension to add Mark As Seen buttons to popular websites.',
-            'url' => 'www.markers.app'
-        ])
 
-        @include('projects.partials.thumbnail', [
-            'name' => 'Race.my',
-            'role' => 'Founder',
-            'description' => 'A ticketing platform for sport events.',
-            'url' => 'www.race.my',
-        ])
+        <div class="flex flex-wrap -mx-4">
+            @include('projects.partials.thumbnail', [
+                'name' => 'Race.my',
+                'role' => 'Founder',
+                'description' => 'Ticketing platform for sport events.',
+                'url' => 'www.race.my',
+            ])
 
-        @include('projects.partials.thumbnail', [
-            'name' => 'Scismic',
-            'role' => 'Tech Lead',
-            'description' => 'A skills-based job matching platform for scientists by scientists.',
-            'url' => 'www.scismic.com',
-        ])
+            @include('projects.partials.thumbnail', [
+                'name' => 'Scismic',
+                'role' => 'Tech Lead',
+                'description' => 'Skills-based job matching platform for scientists by scientists.',
+                'url' => 'www.scismic.com',
+            ])
 
-        @include('projects.partials.thumbnail', [
-            'name' => 'Specs Template',
-            'role' => 'Co-founder',
-            'description' => 'A template to help you write your web project specs in less than 10 minutes.',
-            'url' => 'www.specstemplate.com'
-        ])
+            @include('projects.partials.thumbnail', [
+                'name' => 'Specs Template',
+                'role' => 'Co-founder',
+                'description' => 'Template to help you write your web project specs in less than 10 minutes.',
+                'url' => 'www.specstemplate.com'
+            ])
 
-        @include('projects.partials.thumbnail', [
-            'name' => 'Paywell',
-            'role' => 'Co-founder',
-            'description' => 'A web app to help smart business keep good track of everyone\'s salary.',
-            'url' => 'www.paywell.app'
-        ])
+            @include('projects.partials.thumbnail', [
+                'name' => 'Form \'n Go',
+                'role' => 'Co-founder',
+                'description' => 'Lean and clean form builder built for agencies and freelancers.',
+                'url' => 'www.formngo.app'
+            ])
 
+            @include('projects.partials.thumbnail', [
+                'name' => 'Markers',
+                'role' => 'Creator',
+                'description' => 'Google Chrome extension to add Mark As Seen buttons to popular websites.',
+                'url' => 'www.markers.app'
+            ])
 
+            @include('projects.partials.thumbnail', [
+                'name' => 'Paywell',
+                'role' => 'Co-founder',
+                'description' => 'Web app to help businesses keep good track of everyone\'s salary.',
+                'url' => 'www.paywell.app'
+            ])
+
+            @include('projects.partials.thumbnail', [
+                'name' => 'G Forms Notifications',
+                'role' => 'Creator',
+                'description' => 'Tool for setting up custom email notifications for Google Forms.',
+                'url' => 'googleformsnotifications.code413.io'
+            ])
+        </div>
 
         <h2 class="text-3xl mt-10 mb-2">Open Source</h2>
-        @include('projects.partials.thumbnail', [
-            'name' => 'Sail UI',
-            'description' => 'Basic UI components for Tailwind CSS.',
-            'url' => 'www.github.com/sailui/ui'
-        ])
 
-        @include('projects.partials.thumbnail', [
-            'name' => 'Laravel Survey',
-            'description' => 'Create and manage surveys within your Laravel app.',
-            'url' => 'www.github.com/matt-daneshvar/laravel-survey'
-        ])
+        <div class="flex flex-wrap -mx-4">
 
-        @include('projects.partials.thumbnail', [
-            'name' => 'Eloquent Hashids',
-            'description' => 'Automatically generate and persist Hashids for newly created Eloquent models.',
-            'url' => 'www.github.com/matt-daneshvar/eloquent-hashids'
-        ])
+            @include('projects.partials.thumbnail', [
+                'name' => 'Sail UI',
+                'description' => 'Basic UI components for Tailwind CSS.',
+                'url' => 'www.github.com/sailui/ui'
+            ])
 
-        <div class="mt-5 mb-10"><a href="https://github.com/matt-daneshvar" target="_blank">My Github profile</a> hosts all things I do open source. </div>
+            @include('projects.partials.thumbnail', [
+                'name' => 'Laravel Survey',
+                'description' => 'Create and manage surveys within your Laravel app.',
+                'url' => 'www.github.com/matt-daneshvar/laravel-survey'
+            ])
+
+            @include('projects.partials.thumbnail', [
+                'name' => 'Eloquent Hashids',
+                'description' => 'Automatically generate and persist Hashids for newly created Eloquent models.',
+                'url' => 'www.github.com/matt-daneshvar/eloquent-hashids'
+            ])
+
+            @include('projects.partials.thumbnail', [
+                'name' => 'Laravel Resource Controller',
+                'description' => 'General implementation for Laravel resource controllers.',
+                'url' => 'www.github.com/matt-daneshvar/laravel-resource-controller'
+            ])
+
+            @include('projects.partials.thumbnail', [
+                'name' => 'Laravel Resource Actions',
+                'description' => 'A shorter way to write basic CRUD controller actions in Laravel.',
+                'url' => 'www.github.com/matt-daneshvar/laravel-resource-actions'
+            ])
+        </div>
+
+        <div class="mt-5 mb-10"><a href="https://github.com/matt-daneshvar" target="_blank">My Github profile</a> hosts
+            all things I do open source.
+        </div>
     </div>
 @stop
 
--- resources/views/projects/partials/thumbnail.blade.php
+++ resources/views/projects/partials/thumbnail.blade.php
@@ -1,21 +1,23 @@
-<a href="https://{{ $url }}"
-        class="group border p-5 mb-3 flex items-center border-gray-300 hover:border-red-800 duration-200 transition-all"
-   target="_blank">
-    <div>
-        <div class="flex mb-2 items-center">
-            <h3 class="text-xl font-bold">{{ $name }}</h3>
-            @if(isset($role))
-                <span class="mx-1 text-gray-300">·</span>
-                <span class="">{{ $role  }}</span>
+<div class="w-full px-1 mb-2 flex sm:w-1/2" style="min-height: 12rem">
+    <a href="https://{{ $url }}"
+       class="group w-full border p-5 flex border-gray-300 hover:border-red-800 duration-200 transition-all"
+       target="_blank">
+        <div class="flex flex-col">
+            <div class="flex mb-2 items-center">
+                <h3 class="text-lg font-bold">{{ $name }}</h3>
+                @if(isset($role))
+                    <span class="mx-1 text-gray-300">·</span>
+                    <span class="">{{ $role  }}</span>
+                @endif
+            </div>
+            @if($description ?? null)
+                <p class="text-gray-700 mt-1">{{ $description }}</p>
             @endif
-        </div>
-        @if($description ?? null)
-            <p class="text-gray-700 mt-1">{{ $description }}</p>
-        @endif
 
-        <div class="ml-auto border-gray-100 text-gray-500">{{ $url }}</div>
-    </div>
-    <div class="ml-auto text-red-800 pl-5">
-        <div data-feather="chevron-right" class="transition-all duration-200 transform group-hover:translate-x-2">&gt;</div>
-    </div>
-</a>
+            <div class="mt-auto text-sm border-gray-100 text-gray-500">{{ $url }}</div>
+        </div>
+        <div class="ml-auto mt-auto text-red-800 pl-1">
+            <div data-feather="chevron-right" class="transition-all duration-200 transform group-hover:translate-x-2">&gt;</div>
+        </div>
+    </a>
+</div>
