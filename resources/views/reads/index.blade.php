486141659f622790e1af7e8c3f6e28e5ed60452a
Matt Daneshvar <matt@4thirteen.com.my>
2020-12-22 08:09:22 +0800

    Update reads

--- resources/views/reads/index.blade.php
+++ resources/views/reads/index.blade.php
@@ -11,9 +11,14 @@
         </p>
 
         @include('reads.partials.book', [
+           'title' => 'Homo Deus',
+           'author' => 'Yuval Noah Harari',
+           'current' => true
+        ])
+
+        @include('reads.partials.book', [
            'title' => 'Stillness Is the Key',
            'author' => 'Ryan Holiday',
-           'current' => true
         ])
 
         @include('reads.partials.book', [
